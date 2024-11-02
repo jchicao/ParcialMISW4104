82f91686d4143c239ef5c42404aacbb0105d8eb8
Juan_Chica <j.chicao@uniandes.edu.co>
2024-11-02 11:50:23 -0500

    Contador y web final

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -15,13 +15,30 @@ interface PlantasInteriorExterior{
 export class PlantListComponent implements OnInit {
 
   plants: Array<Plant> = [];
+  PlantasInteriorExterior: PlantasInteriorExterior[] = [];
 
   constructor(private plantService: PlantService) { }
 
   getPlants(){
     this.plantService.getPlants().subscribe((plants) => {
       this.plants = plants;
+      this.contarPlantasInteriorExterior();
     })
+  }
+
+  contarPlantasInteriorExterior(): void {
+    const contador: { [tipoplanta: string]: number } = this.plants.reduce(
+      (acc: { [tipoplanta: string]: number }, plants) => {
+        acc[plants.tipo] = (acc[plants.tipo] || 0) + 1;
+        return acc;
+      },
+      {} as { [tipoplanta: string]: number }
+    );
+
+    this.PlantasInteriorExterior = Object.keys(contador).map((tipoplanta) => ({
+      tipoplanta,
+      total: contador[tipoplanta],
+    }));
   }
 
   ngOnInit() {
